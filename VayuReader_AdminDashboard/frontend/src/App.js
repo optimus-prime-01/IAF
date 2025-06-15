51a67983efc36b137ea0305f9d7c1b6291409c1c
Vasu Singla <120605197+vasusingla545@users.noreply.github.com>
2025-06-15 16:56:26 +0530

    Update App.js

--- VayuReader_AdminDashboard/frontend/src/App.js
+++ VayuReader_AdminDashboard/frontend/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
-import AddDictionaryWords from './components/AddDictionaryWords';
+// import SearchEngine from './components/SearchEngine';
+// import AddDictionary from './components/AddDictionary';
+// import UploadGuide from './components/UploadGuide';
 import AddAbbreviations from './components/AddAbbreviations';
 import AddDictionaryWords from './components/AddDictionaryWords';
-import UploadUserGuide from './components/UploadUserGuide';
+// import UploadUserGuide from './components/UploadUserGuide';
 import UploadForm from './components/UploadForm';
+// import AddDictionaryWords from './components/AddDictionaryWords';
 
 export default function App() {
   return (
@@ -15,11 +18,11 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
        <Route path="/search" element={<UploadForm />} />
-        <Route path="/dictionary" element={<AddDictionaryWords />} />
-        <Route path="/guide" element={<UploadUserGuide />} />
-        <Route path="/abbreviations" element={<AddAbbreviations />} />
+        {/* <Route path="/dictionary" element={<AddDictionaryWords />} /> */}
+        {/* <Route path="/guide" element={<UploadUserGuide />} /> */}
+        <Route path="/add-abbreviations" element={<AddAbbreviations />} />
+ <Route path="/add-dictionary-words" element={<AddDictionaryWords />} />
 
-        <Route path="/add-dictionary-words" element={<AddDictionaryWords />} />
       </Routes>
     </BrowserRouter>
   );
