889c7a0a475629c60125fc4ad4c91fba6f3fde4d
Vasu Singla <120605197+vasusingla545@users.noreply.github.com>
2025-06-16 23:53:00 +0530

    Update DictionaryWord.js to match the schema

--- VayuReader_AdminDashboard/backend/models/DictionaryWord.js
+++ VayuReader_AdminDashboard/backend/models/DictionaryWord.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
+const MeaningSchema = new mongoose.Schema({
+partOfSpeech: { type: String, default: null },
+definition: { type: String, required: true },
+synonyms: { type: [String], default: null },
+examples: { type: [String], default: null },
+});
+
 const DictionaryWordSchema = new mongoose.Schema({
-  word: { type: String, required: true },
-  meaning: { type: String, required: true },
+word: { type: String, required: true, unique: true },
+meanings: { type: [MeaningSchema], required: true },
+synonyms: { type: [String], default: null },
+antonyms: { type: [String], default: null },
 });
 
 module.exports = mongoose.model('DictionaryWord', DictionaryWordSchema);
